ee7c8fb12e2eb6cbcdac01ece2bda6508590c567
Amin Azirani <aminakbari@live.com>
2022-10-27 22:09:17 +0200

    Added some basic necessary files for the sprinkle

---
+++ app/sprinkles/welcome_guide/locale/en_US/messages.php
@@ -0,0 +1,42 @@
+<?php
+/**
+* US English message token translations for the 'welcome guide' sprinkle.
+*
+* @package userfrosting\i18n\en_US
+*/
+
+return [
+    "QUESTION" => [
+        1 => "Question",
+        2 => "Questions",
+        
+        "PAGE" => "A listing of all questions",
+        "CREATE" => "New Question",
+        "CREATED" => "Question <strong>{{name}}</strong> has been successfully created",
+        
+        "SELECTED" => [
+            1 => "Selected Question",
+            2 => "Selected Questions"
+        ],
+        "DELETE" => "Delete Question",
+        "DELETE_CONFIRM"    => "Are you sure you want to delete this question?",
+        "DELETE_YES"        => "Yes, delete question",
+        
+        "DETAILS_UPDATED"=> "Details updated for question <strong>{{name}}</strong>",
+        
+        "DELETION_SUCCESSFUL"   => "Question <strong>{{name}}</strong> has been successfully deleted.",
+        
+        "CLONE" => "Clone Question",
+        "EDIT" => "Edit Question"
+    ],
+    "TITLE" => "Title",
+    "SUB_TITLE" => "Sub Title",
+    "TYPE" => "Type",
+    "AXIS_COUNT" => "Axis Count",
+    "IS_MULTIPLE_CHOICE" => "Multiple Choice",
+    "INFO_URL" => "Info URL",
+    "INFO_DESCRIPTION" => "Info Description",
+    "ANSWER_REQUIRED" => "Answer is Required",
+    "ANSWERS_SELECTED_BY_DEFAULT" => "Answers are selected by Default",
+    "STEP" => "Step"
+    ];

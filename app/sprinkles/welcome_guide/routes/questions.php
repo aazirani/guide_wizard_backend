462a9ec00daf5051ba38d31ad803d6b47166d5d7
Amin Azirani <aminakbari@live.com>
2022-10-27 23:02:51 +0200

    Fixed typo in routes file

--- app/sprinkles/welcome_guide/routes/questions.php
+++ app/sprinkles/welcome_guide/routes/questions.php
@@ -1,17 +1,17 @@
 <?php
 
 $app->group('/questions', function () {
-    $this->get('', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\questionController:pageList')
+    $this->get('', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\QuestionController:pageList')
         ->setName('view_questions');
 })->add('authGuard');
 
 $app->group('/api/questions', function () {
-    $this->get('', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\questionController:getList');
-	$this->post('', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\questionController:create');
-	$this->get('/new', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\questionController:createForm');
+    $this->get('', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\QuestionController:getList');
+	$this->post('', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\QuestionController:create');
+	$this->get('/new', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\QuestionController:createForm');
     /* DELETE */
-    $this->delete('/{question_id:[0-9]+}', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\questionController:delete');
+    $this->delete('/{question_id:[0-9]+}', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\QuestionController:delete');
 	/* EDIT */
-	$this->get('/{question_id:[0-9]+}/edit', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\questionController:editForm');
-	$this->post('/{question_id:[0-9]+}', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\questionController:update');
+	$this->get('/{question_id:[0-9]+}/edit', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\QuestionController:editForm');
+	$this->post('/{question_id:[0-9]+}', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\QuestionController:update');
 })->add('authGuard');

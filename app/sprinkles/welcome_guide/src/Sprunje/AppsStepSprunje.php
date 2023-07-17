f20d9582cb62d797c2caa2a4c137f2e9ee7063ee
Amin Azirani <aminakbari@live.com>
2023-07-17 18:13:24 +0200

    Added special api for app's steps

---
+++ app/sprinkles/welcome_guide/src/Sprunje/AppsStepSprunje.php
@@ -0,0 +1,29 @@
+<?php
+namespace UserFrosting\Sprinkle\WelcomeGuide\Sprunje;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use UserFrosting\Sprinkle\Core\Facades\Debug;
+use UserFrosting\Sprinkle\Core\Sprunje\Sprunje;
+
+/**
+ * Implements Sprunje for the module API.
+ *
+ * @author Amin Akbari (https://github.com/aminakbari)
+ */
+class AppsStepSprunje extends ExtendedSprunje
+{
+
+    protected $name = 'steps';
+
+    /**
+     * Set the initial query used by your Sprunje.
+     */
+    protected function baseQuery()
+    {
+        $query = $this->classMapper->createInstance('step');
+		
+        return $query->joinCreator();
+
+    }
+
+}

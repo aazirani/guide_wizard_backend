49c7406abea46c8d18886c0ba8ad2faa94b86254
Amin Azirani <aminakbari@live.com>
2023-07-25 00:00:37 +0200

    Added support for delivering content based on answers

--- app/sprinkles/welcome_guide/routes/app.php
+++ app/sprinkles/welcome_guide/routes/app.php
@@ -1,6 +1,6 @@
 <?php
 $app->group('/api/app', function () {
     $this->get('/translations', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\AppController:getTranslations');
-    $this->get('/content', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\AppController:getStepList');
+    $this->get('/content/answerIds[/{answerIds:.*}]', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\AppController:getStepList');
     $this->get('/lastUpdates', 'UserFrosting\Sprinkle\WelcomeGuide\Controller\AppController:getLastUpdatedAt');
 });
